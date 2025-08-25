0e13cc9e13bf42e0a640934d7016c98e17aa6684
Drazen567 <157814505+Drazen567@users.noreply.github.com>
2025-08-26 00:03:47 +0200

    Update pic.js

--- pic.js
+++ pic.js
@@ -3,16 +3,20 @@ dpic1.onclick = function() {
   link.href = 'viber_image_2025-08-25_23-10-50-371.jpg';
   link.download = 'slika1.jpg';
   link.click();
+  console.log("downloading...")
 };
 dpic2.onclick = function() {
   let link = document.createElement('a');
   link.href = 'viber_image_2025-08-25_23-10-50-396.jpg';
   link.download = 'slika2.jpg';
   link.click();
+   console.log("downloading...")
 };
 dpic3.onclick = function() {
   let link = document.createElement('a');
   link.href = 'viber_image_2025-08-25_23-10-50-550.jpg';
   link.download = 'slika3.jpg';
   link.click();
+   console.log("downloading...")
 };
+
